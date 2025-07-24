b80bb9430c037cb75f56ba2d26ac9d7bd6b30adc
savileaf <nischal@savileaf.com>
2025-07-24 13:08:46 +0545

    last change

--- project/settings.py
+++ project/settings.py
@@ -130,8 +130,8 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    # "http://127.0.0.1:5500",
-    # "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
     'https://formbuilder-hbbw.onrender.com',
 ]
 
